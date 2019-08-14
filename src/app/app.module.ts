9fc2a064582eda8fea850615196978973efc714c
Steve Ayers <steveneayers@gmail.com>
2019-08-13 19:57:03 -0500

    ngx-drupal7-services
    Added ngx-drupal7-services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  Drupal7ServicesModule,
+  DrupalConstants,
+  Settings
+} from 'ngx-drupal7-services';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    const drupalSettings: Settings = {
+      apiEndPoint: 'api',
+      apiHost: 'localhost',
+      apiProtocol: 'http',
+      language: 'und',
+      requestTimeout: 5000,
+      allowOffline: true // optional
+    };
+    DrupalConstants.Settings = drupalSettings;
+  }
+}
